7ce4ea5a43ab2797565ff0c071c32115ed8a7ed3
QuickZam <111820344+QuickZam@users.noreply.github.com>
2023-08-26 16:43:42 +0000

    added loggers to app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from potassium import Potassium, Request, Response
 from stable_whisper import modify_model 
 from pytube import YouTube
+from logger import logger 
 import torch, whisper, os, base64, urllib.request, json
 
 
@@ -26,6 +27,7 @@ def init():
 def handler(context: dict, request: Request) -> Response:
     link = request.json.get("link")
     model = context.get("model")
+    logger.info(f"Inputs: link=> {link}")
 
     if 'tinyurl' in link: 
         path = urllib.request.urlretrieve(link, f"{link.split('/')[-1]}.mp4")[0]
@@ -33,20 +35,18 @@ def handler(context: dict, request: Request) -> Response:
     else:
         yt = YouTube(link)
         path = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
+        logger.info("It's youtube video!! ")
 
     # translate_options = dict(task="translate", suppress_silence=True, ts_num=16, lower_quantile=0.05, lower_threshold=0.1)4
     translate_options = dict(task="translate",max_initial_timestamp=None)
     result = model.transcribe(path, **translate_options)
-    print(result)
-    print(type(result))
+
+    logger.info("Prediction done!!"); logger.info(f"Type of the result: {type(result)}") 
+    logger.info(f"Original Output: \n{result}")
 
     os.remove(path)
 
-
-    return Response(
-        json = {"outputs": json.dumps(result)}, 
-        status=200
-    )
+    return result 
 
 if __name__ == "__main__":
     app.serve()
