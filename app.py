dc32e802af7c3dbf364d67e84d35c99872984f81
QuickZam <111820344+QuickZam@users.noreply.github.com>
2023-08-26 07:07:50 +0000

    added necessary changes

---
+++ app.py
@@ -0,0 +1,48 @@
+from potassium import Potassium, Request, Response
+from stable_whisper import modify_model 
+from pytube import YouTube
+import torch, whisper, os, base64, urllib.request
+
+
+
+app = Potassium("my_app")
+
+# @app.init runs at startup, and loads models into the app's context
+@app.init
+def init():
+    model = whisper.load_model("large-v1")
+    modify_model(model)
+   
+    context = {
+        "model": model
+    }
+
+    return context
+
+    
+
+# @app.handler runs for every call
+@app.handler("/")
+def handler(context: dict, request: Request) -> Response:
+    link = request.json.get("link")
+    model = context.get("model")
+
+    if 'tinyurl' in link: 
+        path = urllib.request.urlretrieve(link, f"{link.split('/')[-1]}.mp4")[0]
+
+    else:
+        yt = YouTube(link)
+        path = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
+
+    translate_options = dict(task="translate", suppress_silence=True, ts_num=16, lower_quantile=0.05, lower_threshold=0.1)
+    result = model.transcribe(path, **translate_options)
+    os.remove(path)
+
+
+    return Response(
+        json = {"outputs": result}, 
+        status=200
+    )
+
+if __name__ == "__main__":
+    app.serve()
